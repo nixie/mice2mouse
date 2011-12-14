b44df90c05be78edbeb1a43278affa55e1b5c49f
Paulik Miroslav <xpauli00@merlin.fit.vutbr.cz>
2011-12-14 01:53:35 +0100

    pridan soubor statistik, delta_correct.py nyni dela upravenou statistiku ze vsech logu v logs/

--- delta_correct.py
+++ delta_correct.py
@@ -1,16 +1,37 @@
 #!/usr/bin/env python
 
 import sys
+import os
+import string
 
-last_correct = 0
+minval = 0
+pole = []
+x = open('stats/stats.txt', 'w')
+filecounter = 0
 
-for line in sys.stdin.readlines():
-    if line[0] == '#':
-        continue
-    vals = line.split()
-    if len(vals) < 2:
-        continue
-    if vals[1] == '1':
-        print(float(vals[0]) - last_correct)
-        last_correct = float(vals[0])
+for dirname, dirnames, filenames in os.walk('./logs'):
+    for filename in filenames:
+        filecounter += 1
+	last_correct = 0
+	count = 0
+	f = open(os.path.join(dirname, filename))
+	for line in f:
+	    if line[0] == '#':
+		continue
+	    vals = line.split()
+	    if len(vals) < 2:
+		continue
+	    if vals[1] == '1':
+		if minval == 0:
+		    pole.append(float(vals[0]) - last_correct)
+                elif count < minval:
+		    pole[count] += float(vals[0]) - last_correct
+		last_correct = float(vals[0])
+	        count += 1
+	if minval == 0 or count < minval:
+	    minval = count
+for line in pole:
+    s = str(line / filecounter)
+    x.write(s)
+    x.write('\n')
 
